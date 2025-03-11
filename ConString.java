0d8526b2257b086f8323e41590ac749dce96abfd
AvinJ272 <avinjoseph65@gmail.com>
2025-03-11 01:54:35 -0700

    Add files via upload

---
+++ ConString.java
@@ -0,0 +1,13 @@
+class ConString {
+    public static void main(String[] args) {
+        
+        String s1 = "hello";
+        String s2 = "world";
+        String s3 = s1.concat(s2);
+        System.out.println(s3); 
+        
+        
+        String s4 = "good" + "morning";
+        System.out.println(s4); 
+    }
+}
