76bfd9eb6406cbd1008c2aa72583876f609c4090
AvinJ272 <avinjoseph65@gmail.com>
2025-03-11 01:54:15 -0700

    Add files via upload

---
+++ StartString.java
@@ -0,0 +1,12 @@
+class StartString {
+    public static void main(String[]args){
+        String s1="good mmorning";
+        System.out.println(s1);
+        System.out.println("to uppercase:"+s1.toUpperCase());
+        System.out.println("\n To lowercase:"+s1.toLowerCase());
+    
+        
+
+        
+    }
+}
